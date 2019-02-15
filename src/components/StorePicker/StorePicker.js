16ef6e3f9b7c80a44fabece27358e824387582d7
Dominik Manowski <manowski@protonmail.com>
2019-02-15 21:02:58 +0100

    Changing pages with React Router, handling events

--- src/components/StorePicker/StorePicker.js
+++ src/components/StorePicker/StorePicker.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { getFunName } from "../../helpers";
-import { get } from "http";
 
 class StorePicker extends Component {
+  myInput = React.createRef();
+  goToStore = e => {
+    e.preventDefault();
+    // Get the value from the input via ref
+    const storeName = this.myInput.current.value;
+    // Change the view to store via react router push() method
+    this.props.history.push(`store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
