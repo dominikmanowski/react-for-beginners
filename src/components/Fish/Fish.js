3b9dcf5a633292e6568f98d6007f58df5984dede
Dominik Manowski <manowski@protonmail.com>
2019-02-18 11:37:25 +0100

    Refactor Fish to class component

--- src/components/Fish/Fish.js
+++ src/components/Fish/Fish.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import { formatPrice } from "../../helpers";
 
-const Fish = ({ details }) => (
-  <li className="menu-fish">
-    <img src={details.image} alt={details.name} />
-    <h3 className="fish-name">
-      {details.name}
-      <span className="price">{formatPrice(details.price)}</span>
-    </h3>
-    <p>{details.desc}</p>
-    <button>Add To Cart</button>
-  </li>
-);
+class Fish extends Component {
+  render() {
+    const { image, name, price, desc, status } = this.props.details;
+
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+      </li>
+    );
+  }
+}
 
 export default Fish;
